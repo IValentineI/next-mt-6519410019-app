f8924f775913835be076ba8d361d446c5fb75b75
IValentineI <S6519410019@sau.ac.th>
2026-02-26 19:09:13 +0700

    หน้า Sidemenu เสร็จแล้ว

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import MyCard from "@/components/MyCard"
+import SideMenu from "@/components/SideMenu"
 
 export default function Home() {
   return (
     <div>
       <MyCard />
+      <SideMenu />
     </div>
   )
 }
